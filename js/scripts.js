978b70046e3b9c666b29accd51480ca07c4f836c
Kenny Wolfenberger and Tanner Damron <tanner@>
2018-10-16 14:41:18 -0700

    add return to original term function and clickable text and some styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,19 @@ $(document).ready(function() {
     $("#js").toggle('slow');
   });
 
+  $(".jsp").click(function() {
+    $("#js").toggle('slow');
+    $("#jsp").toggle('slow');
+  });
+
   $("#boolean").click(function() {
     $("#booleanp").toggle('slow');
     $("#boolean").toggle('slow');
+  });
+
+  $(".booleanp").click(function() {
+    $("#boolean").toggle('slow');
+    $("#booleanp").toggle('slow');
   });
 
   $("#jquery").click(function() {
@@ -15,9 +25,19 @@ $(document).ready(function() {
     $("#jquery").toggle('slow');
   });
 
+  $(".jqueryp").click(function() {
+    $("#jquery").toggle('slow');
+    $("#jqueryp").toggle('slow');
+  });
+
   $("#functions").click(function() {
     $("#functionsp").toggle('slow');
     $("#functions").toggle('slow');
+  });
+
+  $(".functionsp").click(function() {
+    $("#functions").toggle('slow');
+    $("#functionsp").toggle('slow');
   });
 
   $("#strings").click(function() {
@@ -25,9 +45,19 @@ $(document).ready(function() {
     $("#strings").toggle('slow');
   });
 
+  $(".stringsp").click(function() {
+    $("#strings").toggle('slow');
+    $("#stringsp").toggle('slow');
+  });
+
   $("#nan").click(function() {
     $("#nanp").toggle('slow');
     $("#nan").toggle('slow');
+  });
+
+  $(".nanp").click(function() {
+    $("#nan").toggle('slow');
+    $("#nanp").toggle('slow');
   });
 
 });
