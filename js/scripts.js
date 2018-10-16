1ae167efb7bac4b3e5a5f32700cb09e83ec80f05
Kenny Wolfenberger and Tanner Damron <tanner@>
2018-10-16 14:20:57 -0700

    add flashcards and click elements for paragraphs

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+  $("#js").click(function() {
+    $("#jsp").toggle('slow');
+    $("#js").toggle('slow');
+  });
+
+  $("#boolean").click(function() {
+    $("#booleanp").toggle('slow');
+    $("#boolean").toggle('slow');
+  });
+
+  $("#jquery").click(function() {
+    $("#jqueryp").toggle('slow');
+    $("#jquery").toggle('slow');
+  });
+
+  $("#functions").click(function() {
+    $("#functionsp").toggle('slow');
+    $("#functions").toggle('slow');
+  });
+
+  $("#strings").click(function() {
+    $("#stringsp").toggle('slow');
+    $("#strings").toggle('slow');
+  });
+
+  $("#nan").click(function() {
+    $("#nanp").toggle('slow');
+    $("#nan").toggle('slow');
+  });
+
+});
